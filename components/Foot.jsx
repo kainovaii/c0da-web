a3e4a9ebbcf3266ed381fb6e8bd7f8125a26b9f6
Tom Michelon <baron.tom.michelon@gmail.com>
2022-10-21 14:41:55 +0200

    🛠️ Fix

--- components/Foot.jsx
+++ components/Foot.jsx
@@ -1,11 +1,13 @@
 export default function Foot() {
     return (
         <>
+        {/*
         <script src="/js/lib/jquery.min.js" async></script>
         <script src="/js/lib/svg4everybody.min.js" async></script>
         <script src="/js/lib/owl.carousel.min.js" async></script>
         <script src="/js/lib/jquery.magnific-popup.min.js" async></script>
         <script src="/js/lib/aos.js" async></script>
+        */}
         <script src="/js/app.js" async></script>
         </>
     )
